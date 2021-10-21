51f23e8e322e35b63a8dbd7a56dc6708835447f5
DevCaress <daniel.enriquez.lucas@gmail.com>
2021-10-20 21:24:59 -0500

    js 1

---
+++ js/clase 1/main.js
@@ -0,0 +1,53 @@
+/* console.log('Hello World'); */
+
+
+/* var cualquiera = 938471;
+var puntoDecimal = 098098908.123
+var numeroNegativo = -123123;
+var string = 'Hola mundo';
+
+console.log(cualquiera);
+console.log(puntoDecimal);
+console.log(numeroNegativo);
+console.log(string); */
+
+
+/* var loQueSea = 30;
+console.log(loQueSea);
+
+
+loQueSea = 'Gato';
+console.log(loQueSea);
+
+
+loQueSea = -1298172398;
+console.log(loQueSea); */
+
+/* var equis = 'asdkjahsdkj';
+var equis2 = equis;
+var equis3 = equis2;
+
+console.log(equis);
+console.log(equis2);
+console.log(equis3); */
+
+
+/* operadores matematicos */
+
+// + * - / %
+
+var num1 = 15;
+var num2 = 2;
+var suma = num1 % num2;
+
+/* console.log(suma); */
+
+
+
+/* operadores texto */
+
+var name = 'pedro ';
+var lastName = 'perez ';
+
+
+console.log('Bienvenido ' + name + lastName + (-120398123));
---
+++ js/clase 1/main2.js
@@ -0,0 +1,26 @@
+var equis1 = '15';
+var equis2 = 15;
+var nombre = '';
+/* operadores comparacion */
+
+
+/* 
+equis1 == equis2 
+equis1 === equis2
+equis1 > equis2
+equis1 < equis2*/
+
+
+if( equis1 === equis2 ){
+    console.log('Bienvenido');
+}
+
+
+
+nombre = prompt('Cual es tu nombre??');
+
+
+/* crear un algoritmo que le pida al usario su nombre y su edad
+y si es mayor de edad te dara un mensaje de bienvenida con un alert
+
+*/
